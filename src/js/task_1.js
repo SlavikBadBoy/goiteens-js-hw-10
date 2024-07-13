3939793892243b3e7504e426f10198f50e39af97
SlavikBadBoy <buracenkoslaviK@gmail.com>
2024-07-13 11:37:43 +0300

    10

---
+++ src/js/task_1.js
@@ -0,0 +1,6 @@
+// const button = document.getElementById("myButton");
+
+// // Використання стрілкової функції як колбек
+// button.addEventListener("click", () => {
+//   alert("Hello World!");
+// });
---
+++ src/js/task_2.js
@@ -0,0 +1,16 @@
+// const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// const checkGuess = () => {
+//   const guess = parseInt(document.getElementById("guessField").value);
+
+//   if (guess === randomNumber) {
+//     document.getElementById("guessResult").textContent =
+//       "Вітаємо! Ви вгадали число!";
+//   } else if (guess < randomNumber) {
+//     document.getElementById("guessResult").textContent =
+//       "Ваше число занадто маленьке. Спробуйте ще раз.";
+//   } else {
+//     document.getElementById("guessResult").textContent =
+//       "Ваше число занадто велике. Спробуйте ще раз.";
+//   }
+// };
